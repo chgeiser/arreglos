9bb90b1eaabf178d910c46a37873e34c26672a35
Christopher Geiser <christopher.geiser@gmail.com>
2025-10-21 22:32:07 -0300

    objetos

---
+++ assets/js/index.js
@@ -0,0 +1,65 @@
+const propiedades_ventas = [
+    {
+        nombre:"Apartamento de Lujo en zona Exclusiva",
+		src:"../img/propiedad_Ven_1.png",
+		descripcion:"Este apartamento de lujo esta ubicado en una exclusiva zona residencial",
+		ubicacion:"123 Luxury Lane, Prestige Suburb, CA 45678",
+		habitaciones:4,
+		costo:5000,
+		smoke:false,
+		pets:false
+    },
+    {
+        nombre:"Apartamento acogedor en la Montañas",
+		src:"../img/propiedad_Ven_2.png",
+		descripcion:"Este apartamento acogedor está situado en lo alto de una montaña con impresionante vista",
+		ubicacion:"789 Mountain Road, Summit Peaks, CA 23456",
+		habitaciones:2,
+		costo:1200,
+		smoke:true,
+		pets:true
+    },
+    {
+        nombre:"Penthouse de lujo con terraza panorámica",
+		src:"../img/propiedad_Ven_3.png",
+		descripcion:"Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+		ubicacion:"567 Skyline Avenue, Skyview City, CA 56789",
+		habitaciones:3,
+		costo:4500,
+		smoke:false,
+		pets:true
+    }
+]
+
+const propiedades_alquiler = [
+    {
+        nombre:"",
+		src:"",
+		descripcion:"",
+		ubicacion:"",
+		habitaciones:0,
+		costo:0,
+		smoke:false,
+		pets:true
+    },
+    {
+        nombre:"",
+		src:"",
+		descripcion:"",
+		ubicacion:"",
+		habitaciones:0,
+		costo:0,
+		smoke:false,
+		pets:true
+    },
+    {
+        nombre:"",
+		src:"",
+		descripcion:"",
+		ubicacion:"",
+		habitaciones:0,
+		costo:0,
+		smoke:false,
+		pets:true
+    }
+]
