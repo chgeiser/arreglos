65b32eb20ec209c35bd60773e94291ec986e296b
Christopher Geiser <christopher.geiser@gmail.com>
2025-10-22 22:16:29 -0300

    mejoras

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,79 +1,83 @@
 const propiedades_ventas = [
-    {
-        nombre:"Apartamento de Lujo en zona Exclusiva",
-		src:"https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-		descripcion:"Este apartamento de lujo esta ubicado en una exclusiva zona residencial",
-		ubicacion:"123 Luxury Lane, Prestige Suburb, CA 45678",
-		habitaciones:4,
-        banos: 4,
-		costo:5000,
-		smoke:false,
-		pets:false
-    },
-    {
-        nombre:"Apartamento acogedor en la Montañas",
-		src:"https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
-		descripcion:"Este apartamento acogedor está situado en lo alto de una montaña con impresionante vista",
-		ubicacion:"789 Mountain Road, Summit Peaks, CA 23456",
-		habitaciones:2,
-        banos: 1,
-		costo:1200,
-		smoke:true,
-		pets:true
-    },
-    {
-        nombre:"Penthouse de lujo con terraza panorámica",
-		src:"https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-		descripcion:"Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
-		ubicacion:"567 Skyline Avenue, Skyview City, CA 56789",
-		habitaciones:3,
-        banos: 3,
-		costo:4500,
-		smoke:false,
-		pets:true
-    }
-]
+  {
+    nombre: "Apartamento de Lujo en zona Exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo esta ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    banos: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento acogedor en la Montañas",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionante vista",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    banos: 1,
+    costo: 1200,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: 3,
+    banos: 3,
+    costo: 4500,
+    smoke: false,
+    pets: true,
+  },
+];
 
 const propiedades_alquiler = [
-    {
-        nombre:"Apartamento en el centro de la ciudad",
-		src:"https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-		descripcion:"Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo",
-		ubicacion:"123 Main Street, Anytown, CA 91234",
-		habitaciones:2,
-        banos: 2,
-		costo:2000,
-		smoke:false,
-		pets:true
-    },
-    {
-        nombre:"Apartamento luminoso con vista al mar",
-		src:"https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-		descripcion:"Este hermoso apartamento ofrece una vista impresionante al mar",
-		ubicacion:"456 Ocean Avenue, Seaside Town, CA 56789",
-		habitaciones:3,
-        banos: 3,
-		costo:2500,
-		smoke:true,
-		pets:true
-    },
-    {
-        nombre:"Condominio moderno en zona Resindencial",
-		src:"https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
-		descripcion:"Este elegante condominio moderno esta ubicado en una tranquila zona residencial",
-		ubicacion:"123 Main Street, Anytown, CA 91234",
-		habitaciones:2,
-        banos: 2,
-		costo:2200,
-		smoke:false,
-		pets:false
-    }
-]
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    descripcion:
+      "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    banos: 2,
+    costo: 2000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento luminoso con vista al mar",
+    src: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    descripcion:
+      "Este hermoso apartamento ofrece una vista impresionante al mar",
+    ubicacion: "456 Ocean Avenue, Seaside Town, CA 56789",
+    habitaciones: 3,
+    banos: 3,
+    costo: 2500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Condominio moderno en zona Resindencial",
+    src: "https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    descripcion:
+      "Este elegante condominio moderno esta ubicado en una tranquila zona residencial",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    banos: 2,
+    costo: 2200,
+    smoke: false,
+    pets: false,
+  },
+];
 
-
-function mostrarPropiedad(tipoPropiedad){
-
-    let html = `
+function mostrarPropiedad(tipoPropiedad) {
+  let html = `
     <!DOCTYPE html>
     <html lang="en">
   <head>
@@ -133,7 +137,7 @@ function mostrarPropiedad(tipoPropiedad){
     <div class="row">
     `;
 
-    let final= `
+  let final = `
     </section>
             </div>
     <script
@@ -144,17 +148,15 @@ function mostrarPropiedad(tipoPropiedad){
     </body>
 </html>`;
 
+  let textoListaVenta = "";
+  let textoListaAlquiler = "";
 
-    let textoListaVenta="";
-    let textoListaAlquiler="";
+  //const ventas = document.querySelector('#venta');
+  //const alquiler = document.querySelector('#alquiler');
 
-    //const ventas = document.querySelector('#venta');
-    //const alquiler = document.querySelector('#alquiler');
-
-    if(tipoPropiedad === "ventas"){
-
-        for(let propiedadVenta of propiedades_ventas){
-        textoListaVenta += `
+  if (tipoPropiedad === "ventas") {
+    for (let propiedadVenta of propiedades_ventas) {
+      textoListaVenta += `
         <div class="col-md-4 mb-4">
         <div class="card">
               <img
@@ -170,39 +172,45 @@ function mostrarPropiedad(tipoPropiedad){
                   ${propiedadVenta.descripcion}
                 </p>
                 <p>
-                  <i class="fas fa-map-marker-alt"></i> ${propiedadVenta.ubicacion}
+                  <i class="fas fa-map-marker-alt"></i> ${
+                    propiedadVenta.ubicacion
+                  }
                 </p>
                 <p>
-                  <i class="fas fa-bed"></i> ${propiedadVenta.habitaciones} Habitaciones |
+                  <i class="fas fa-bed"></i> ${
+                    propiedadVenta.habitaciones
+                  } Habitaciones |
                   <i class="fas fa-bath"></i> ${propiedadVenta.banos} Baños
                 </p>
-                <p><i class="fas fa-dollar-sign"></i> ${propiedadVenta.costo}</p>
-                ${propiedadVenta.smoke 
-                        ? `<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar</p>` 
-                        : `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite fumar</p>`}
-                 ${propiedadVenta.pets 
-                        ? `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>` 
-                        : `<p class="text-danger"><i class="fas fa-ban"></i> No se permiten mascotas</p>`}
+                <p><i class="fas fa-dollar-sign"></i> ${
+                  propiedadVenta.costo
+                }</p>
+                ${
+                  propiedadVenta.smoke
+                    ? `<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar</p>`
+                    : `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite fumar</p>`
+                }
+                 ${
+                   propiedadVenta.pets
+                     ? `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                     : `<p class="text-danger"><i class="fas fa-ban"></i> No se permiten mascotas</p>`
+                 }
               </div>
             </div>
             </div>
             `;
-
-            
     }
 
-    let paginaVentas = html+textoListaVenta+final;
+    let paginaVentas = html + textoListaVenta + final;
     console.log(paginaVentas);
-    
-    const nuevaVentana = window.open("propiedades_ventas", "_blank");
+
+    const nuevaVentana = window.open("", "_blank");
     nuevaVentana.document.write(paginaVentas);
     nuevaVentana.document.close();
 
-    }else if(tipoPropiedad === "alquiler"){
-
-        for(let propiedadAlquiler of propiedades_alquiler){
-
-        textoListaAlquiler += `
+  } else if (tipoPropiedad === "alquiler") {
+    for (let propiedadAlquiler of propiedades_alquiler) {
+      textoListaAlquiler += `
         <div class="col-md-4 mb-4">
         <div class="card">
               <img
@@ -218,41 +226,45 @@ function mostrarPropiedad(tipoPropiedad){
                   ${propiedadAlquiler.descripcion}
                 </p>
                 <p>
-                  <i class="fas fa-map-marker-alt"></i> ${propiedadAlquiler.ubicacion}
+                  <i class="fas fa-map-marker-alt"></i> ${
+                    propiedadAlquiler.ubicacion
+                  }
                 </p>
                 <p>
-                  <i class="fas fa-bed"></i> ${propiedadAlquiler.habitaciones} Habitaciones |
+                  <i class="fas fa-bed"></i> ${
+                    propiedadAlquiler.habitaciones
+                  } Habitaciones |
                   <i class="fas fa-bath"></i> ${propiedadAlquiler.banos} Baños
                 </p>
-                <p><i class="fas fa-dollar-sign"></i> ${propiedadAlquiler.costo}</p>
+                <p><i class="fas fa-dollar-sign"></i> ${
+                  propiedadAlquiler.costo
+                }</p>
                 
-                 ${propiedadAlquiler.smoke 
-                        ? `<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar</p>` 
-                        : `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite fumar</p>`}
+                 ${
+                   propiedadAlquiler.smoke
+                     ? `<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar</p>`
+                     : `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite fumar</p>`
+                 }
 
-                ${propiedadAlquiler.pets 
-                    ? `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>` 
-                    : `<p class="text-danger"><i class="fas fa-ban"></i> No se permiten mascotas</p>`}
+                ${
+                  propiedadAlquiler.pets
+                    ? `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                    : `<p class="text-danger"><i class="fas fa-ban"></i> No se permiten mascotas</p>`
+                }
                 
               </div>
             </div>
             </div>
             `;
-
-
     }
-    let paginaAlquiler = html+textoListaAlquiler+final;
-    const nuevaVentana = window.open("propiedades_alquiler", "_blank");
+    let paginaAlquiler = html + textoListaAlquiler + final;
+    const nuevaVentana = window.open("", "_blank");
     nuevaVentana.document.write(paginaAlquiler);
     nuevaVentana.document.close();
-    }else{
-        console.log("error");
-    }
+  } else {
+    console.log("error");
+  }
 
-    //
-    //
-
-   
-    
-
+  //
+  //
 }
